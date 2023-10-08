c0eb55505051057f39f9b1631efc3dcab32ce60e
StefanD <stefan.durla@gmail.com>
2023-10-08 12:21:29 +0300

    Added User class

--- src/main/java/ro/sda/java57/restapi/controller/HelloController.java
+++ src/main/java/ro/sda/java57/restapi/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import ro.sda.java57.restapi.model.User;
 
 @Controller
 @ResponseBody
@@ -18,6 +19,11 @@ public class HelloController {
     @PostMapping
     public ResponseEntity<String> testPost(@RequestBody String message){
         return ResponseEntity.ok("Hello from " + message);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<String> testUser(@RequestBody User user){
+        return ResponseEntity.ok("Hello from " + user);
     }
 
 }
---
+++ src/main/java/ro/sda/java57/restapi/model/User.java
@@ -0,0 +1,10 @@
+package ro.sda.java57.restapi.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private String email;
+    private String lastName;
+}
